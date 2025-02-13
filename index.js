76c10b3fe81397047818a839ef8e545e56ac1015
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-14 00:13:41 +0530

    pushing

--- index.js
+++ index.js
@@ -31,7 +31,16 @@ let posts = [
 app.get("/posts", (req, res) => {
     res.render("index.ejs", {posts});
 });
-
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+    
+});
+app.post("/posts", (req, res) => {
+    //res.render("new.ejs");
+    console.log(req.body);
+    res.send("post request is working");
+    
+});
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
